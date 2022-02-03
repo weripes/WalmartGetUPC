c22ce773dd653d793728172f2d520345956f63b9
weripes <panasicoveduard@gmail.com>
2022-02-03 19:32:27 +0200

    Get UPC

--- ups.py
+++ ups.py
@@ -3,24 +3,18 @@ import requests
 import json
 
 
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
+def walmart_get_upc(url):
+    HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
 
-url = "https://www.walmart.com/ip/Lasko-Limited-Edition-20-Box-Fan-with-3-Speeds-B20610-Red-White-Blue/404928050?athcpid=404928050&athpgid=athenaHomepage&athcgid=dealspage-home-4446685&athznid=ItemCarouselType_BestInDeals&athieid=v1&athstid=CS020&athguid=466001f5-9a18a716-bed495689f15260d&athancid=null&athena=true"
+    req = requests.get(url, headers=HEADERS)
+    html = bs4(req.content, 'html.parser')
+    tag_script = str(html.select('.tb-optimized'))[64:-10]
 
-url2 = 'https://www.walmart.com/ip/Midea-5-000-BTU-115V-Mechanical-Window-Air-Conditioner-MAW05M1WWT/212092810?athcpid=212092810&athpgid=athenaHomepage&athcgid=dealspage-home-4446685&athznid=ItemCarouselType_BestInDeals&athieid=v1&athstid=CS020&athguid=466001f5-9a18a716-89c28f1b9f15260d&athancid=null&athena=true'
-url3 = 'https://www.walmart.com/ip/Midea-7-000-BTU-10-000-BTU-ASHRAE-115V-Portable-Air-Conditioner-with-ComfortSense-Remote-White-MAP07R1WWT/238274032?athcpid=238274032&athpgid=athenaItemPage&athcgid=null&athznid=PWVAV&athieid=v0&athstid=CS020&athguid=182e9217-007-17b06df3f1c89e&athancid=null&athena=true'
-req = requests.get(url3, headers=HEADERS)
+    if tag_script == []:
+        print('\n[!] UPC not found\n')
+        return
 
-html = bs4(req.content, 'html.parser')
+    json_file = json.loads(tag_script)    
+    upc_code = json_file['item']['product']['buyBox']['products'][0]['upc']
 
-
-
-js = str(html.select('.tb-optimized'))[64:-10]
-
-js = json.loads(js)
-
-
-def walmart_get_upc(json_file):
-    return json_file['item']['product']['buyBox']['products'][0]['upc']
-    
-print(walmart_get_upc(js))
+    return upc_code
